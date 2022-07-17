2d6f374c12a73f8f569b5b338a7d8e8ec166b844
eteka03 <eteka03.akpaki@gmail.com>
2022-07-17 03:02:19 +0200

    horizontal wrap to make bunny always comeback to screen

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -98,5 +98,17 @@ export default class Game extends Phaser.Scene{
             //stop to move if no movement
             this.player.setVelocityX(0)
         }
+
+        this.horizontalWrap(this.player)
+    }
+
+    horizontalWrap(sprite){
+        const halfWidth = sprite.displayWidth * 0.5
+        const gameWidth = this.scale.width
+        if (sprite.x < -halfWidth){
+            sprite.x = gameWidth + halfWidth
+        }else if(sprite.x > gameWidth + halfWidth){
+            sprite.x = -halfWidth
+        }
     }
 }
